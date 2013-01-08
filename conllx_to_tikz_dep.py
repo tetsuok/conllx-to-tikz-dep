0894e43ad8b52cf4e8f4eadf45e422d6c19e99a3
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2013-01-09 03:42:18 +0900

    Fix copyright.

--- conllx_to_tikz_dep.py
+++ conllx_to_tikz_dep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2012 Tetsuo Kiso. All rights reserved.
+# Copyright 2012-2013 Tetsuo Kiso. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
